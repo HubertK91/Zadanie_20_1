45501c0190512fae116cc58df81ed5dd005bc509
Hubert Kowalski <hubert160491@gmail.com>
2021-03-24 23:33:01 +0100

    Poprawka

--- src/main/java/pl/hk/zadanie_20_1/UserController.java
+++ src/main/java/pl/hk/zadanie_20_1/UserController.java
@@ -1,6 +1,7 @@
 package pl.hk.zadanie_20_1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,33 +21,31 @@ public class UserController {
     @GetMapping("/users")
     public String displayListUsers() {
         List<User> listUsers = usersRepository.getListUsers();
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (User user : listUsers) {
-            result += user.getFirstName() + " " + user.getLastName() + " wiek: " + user.getAge() + "<br/>";
+            result.append(user);
         }
-        return result;
+        return result.toString();
     }
 
     @GetMapping("/add")
-    public String addUser(@RequestParam String imie, String nazwisko, Integer wiek) {
-        User user = new User(imie, nazwisko, wiek);
-        if (imie == null || imie.isEmpty()){
+    public String addUser(@RequestParam(name = "imie", required = false) String firstName,
+                          @RequestParam(name = "nazwisko") String lastName, @RequestParam(name = "wiek") Integer age) {
+        return createAndAddUser(firstName, lastName, age);
+    }
+
+    @PostMapping("/add")
+    public String addUserViaForm(@RequestParam String firstName, @RequestParam String lastName, @RequestParam Integer age) {
+        return createAndAddUser(firstName, lastName, age);
+    }
+
+    private String createAndAddUser(String firstName, String lastName, Integer age) {
+        if (ObjectUtils.isEmpty(firstName)) {
             return "redirect:/err.html";
-        }else {
+        } else {
+            User user = new User(firstName, lastName, age);
             usersRepository.add(user);
             return "redirect:/success.html";
         }
     }
-
-    @PostMapping("/hello")
-    public String addUserViaForm(@RequestParam String firstName, String lastName, Integer age) {
-        User user = new User(firstName, lastName, age);
-        if (firstName == null || firstName.isEmpty()){
-            return "redirect:/err.html";
-        }else {
-            usersRepository.add(user);
-            return "redirect:/success.html";
-        }
-    }
-
 }
